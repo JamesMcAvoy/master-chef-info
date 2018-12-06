6be5cfdcd9b766914e06b29193832e4919da7332
JamesMcAvoy <mathieu.brisset@laposte.net>
2018-12-06 09:56:17 +0100

    Ajoutez des fichiers projet.

---
+++ master-chef-info-cuisine/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace master_chef_info_cuisine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("test");
+        }
+    }
+}
