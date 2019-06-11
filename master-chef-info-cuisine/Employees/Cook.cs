896a59f0cb3277abc714cb140d41153a6cf0b968
JamesMcAvoy <mathieu.brisset@laposte.net>
2019-06-11 20:23:56 +0200

    socket

---
+++ master-chef-info-cuisine/Employees/Cook.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace master_chef_info_cuisine.Employees
+{
+    class Cook : Employee
+    {
+
+    }
+}
